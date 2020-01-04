6d0b41a107d703afcdfc12a9e1406fae9eba66e9
Evgeny Latkin <yevgeny.latkin@gmail.com>
2020-01-04 09:42:32 +0300

    pretty-print test parameters

--- tests/unit/test_fma.cpp
+++ tests/unit/test_fma.cpp
@@ -8,6 +8,7 @@
 #include <gtest/gtest.h>
 
 #include <iostream>
+#include <string>
 
 #if !defined(TFCP_SIMD_AVX)
 #error TFCP_SIMD_AVX undefined! (please enable AVX2)
@@ -19,16 +20,50 @@
 
 namespace {
 
-template<typename T> void test_fma(const char name[]) {
-    std::cout << "test_fma: " << name << std::endl;
-    T x = 1, y = 2, z = 3;
-    T r = tfcp::fmsub(x, y, z);
-    EXPECT_EQ(r, -1);
-}
+using namespace testing;
 
-TEST(TfcpSimd, Fma) {
-    test_fma<float>("float");
-    test_fma<double>("double");
+class TypeName {
+public:
+    using Type = std::string;
+    Type get() { return value; }
+    TypeName(const char init[]) { value = init; }
+public:
+    friend
+    std::ostream& operator<< (std::ostream& out, const TypeName& typeName) {
+        return out << typeName.value;
+    }
+private:
+    Type value;
+};
+
+class TestUnitFma : public TestWithParam<TypeName> {
+public:
+    template<typename T>
+    static void test_fma(const char name[]) {
+        T x = 1, y = 2, z = 3;
+        T r = tfcp::fmsub(x, y, z);
+        EXPECT_EQ(r, -1);
+    }
+};
+
+TEST_P(TestUnitFma, smoke) {
+    auto param = GetParam();
+    std::string typeName = param.get();
+
+    if (typeName == "float") {
+        test_fma<float>("float");
+    }
+    else if (typeName == "double") {
+        test_fma<double>("double");
+    }
+    else {
+        FAIL();
+    }
 }
 
 } // namespace
+
+INSTANTIATE_TEST_SUITE_P(types, TestUnitFma,
+                         Values(TypeName("float"),
+                                TypeName("double")),
+                         PrintToStringParamName());
